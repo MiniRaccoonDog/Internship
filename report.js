f90910a7ec5a78c3f5c967dd25617bc0a434cd30
Alyssa Torres <atorres@spoonflower.com>
2017-07-28 15:32:16 -0400

    Website full revision

--- report.js
+++ report.js
@@ -1,32 +1,52 @@
-$( ".submission" ).click(function() {
-  $( "#level" ).slideToggle( "slow", function() {
+$( document ).ready(function() {
+  $( ".collapse" ).click(function() {
+    var target = $(this).attr("href");
+    $(target).slideToggle("fast", function(){
+      $(".response").show();
+    });
   });
-  $(".response").show();
-});
+  $( ".reset" ).click(function() {
+    var target = $(this).attr("href");
+    $( target ).slideToggle( "slow", function() {
+    });
 
- function systemCheck() {
-    var system = document.forms[0];
-    var txt = "";
-    var txt2 = document.getElementById("systemfix").value;
-    var i;
-    for (i = 0; i < system.length; i++) {
-        if (system[i].checked) {
-            txt = txt + system[i].value + ", ";
-        }
+  })
+});
+/*For Question ONE*/
+function severityCheck() {
+   var severity = document.forms[0].elements["severity"];
+   var sevtxt = "";
+   var a;
+   for (a = 0; a < severity.length; a++) {
+     if (severity[a].checked) {
+       sevtxt = sevtxt + severity[a].value + " ";
+     }
+   }
+   document.getElementById("priority").innerHTML = "Severity is: " + sevtxt;
+}
+/*For Question TWO*/
+function systemCheck() {
+  var system = document.forms[0].elements["systems"];
+  var txt = "";
+  var txt2 = document.getElementById("systemfix").value;
+  var i;
+  for (i = 0; i < system.length; i++) {
+    if (system[i].checked) {
+      txt = txt + system[i].value + ", ";
     }
+  }
   document.getElementById("rating").innerHTML = "Machines affected are: " + txt + txt2;
 }
-
-$( ".reset" ).click(function() {
-  $( "#level" ).slideToggle( "slow", function() {
-    });
-  function uncheckAll(){
-    $("input[type='checkbox']:checked").prop("checked",false)
-};
-});
-
-/*function showInput() {
-        document.getElementById('display').innerHTML =
-                    document.getElementById("user_input").value;
+/*For Question THREE*/
+function natureCheck() {
+  var nature = document.forms[0].elements["nature"];
+  var txt3 = "";
+  var txt32 = document.getElementById("freenature").value;
+  var c;
+  for (c = 0; c < nature.length; c++) {
+    if (nature[c].checked) {
+      txt3 = txt3 + nature[c].value;
     }
-    */
+  }
+  document.getElementById("naturetype").innerHTML = "The problem appears to be " + txt3 +". "+ txt32;
+}
