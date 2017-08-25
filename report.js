56857bc5fc21db422a1c63f423b1f5770b1f07a6
Alyssa Torres <atorres@spoonflower.com>
2017-08-25 17:32:58 -0400

    Subquestions work, adding table

--- report.js
+++ report.js
@@ -18,6 +18,7 @@ $( document ).ready(function() {
 var saveSeverity="";
 var saveNature="";
 var machineTicket="";
+var machineOther="";
 var natureOtherTicket="";
 var txtwrds="";
 /*For Question ONE*/
@@ -31,22 +32,48 @@ function severityCheck() {
      }
    };
    window.saveSeverity = sevtxt;
-   document.getElementById("priority").innerHTML = "Severity is: " + sevtxt;
+   document.getElementById("priority").innerHTML = "Severity level: " + sevtxt;
+};
+function fullSystemCheck() {
+  resetSystemCheck();
+  systemCheck();
 }
+
+/*Refresh Question TWO*/
+function resetSystemCheck() {
+    $("#rating").empty();
+    $("#rating2").empty();
+  }
+
 /*For Question TWO*/
 function systemCheck() {
   var systems = document.forms[0].elements["systems"];
-  var txt = "";
   var txt2 = document.getElementById("systemsfix").value;
+  var finaltxt="";
   var b;
+  var b2;
   for (b = 0; b < systems.length; b++) {
     if (systems[b].checked) {
-      txt = txt + systems[b].value + ", ";
+      var selector = $(systems[b]).attr("data-target");
+      var subbies = $(selector).find(".subsystems");
+      var txt = systems[b].value
+      $("#rating").append("<h3>"+txt+"</h3><ul></ul>")
+      finaltxt += "<h3>"+txt+"</h3><ul>"
+      for (b2 = 0; b2 < subbies.length; b2++) {
+        if (subbies[b2].checked) {
+          var subtxt = subbies[b2].value
+          $("#rating").append("<li>"+subtxt+"</li>");
+          finaltxt += "<li>"+subtxt+"</li>";
+        }
+      }
+      finaltxt += "</ul>"
     }
   }
-  document.getElementById("rating").innerHTML = "Machines affected are: " + txt + txt2 + ". ";
-  window.machineTicket = txt + txt2;
+  $("#rating2").append("Other: "+txt2);
+  window.machineTicket = finaltxt;
+  window.machineOther= txt2;
 }
+
 /*For Question THREE*/
 function natureCheck() {
   var nature = document.forms[0].elements["nature"];
@@ -59,18 +86,20 @@ function natureCheck() {
     }
   }
   window.saveNature = txt3;
-  document.getElementById("naturetype").innerHTML = "The problem appears to be " + txt3 +". "+ txt32 + ". ";
+  document.getElementById("naturetype").innerHTML = "The problem appears to be " + txt3 + ". " + txt32;
   window.natureOtherTicket = txt32;
 };
-
-/*BOX SELECTION*/
-$()
 
 
 /*For Question FOUR*/
 function textareaCheck() {
      window.txtwrds = document.getElementById("txtbox").value;
    document.getElementById("texttArea").innerHTML = "Additional Notes: " + txtwrds;
+   //document.getElementById("SECRETbox").checked = true;
+   var plz = document.getElementById("SECRETbox");
+   plz.value = "Additional Information: " + natureOtherTicket + ". " + machineOther + ". " + txtwrds;
+   console.log(plz);
+   console.log(document.getElementById("SECRETbox").value);
 };
 
 /*subQuestion Expander*/
@@ -86,26 +115,38 @@ $( document ).ready(function() {
 })
 
 
-/*Okay lets try some bullshit */
+/*Okay lets try some stuff */
 
 function allTogether () {
   var emailEnder = "";
   var emailStart = "";
   var switchTWO = saveSeverity;
   var switchONE = saveNature;
+  console.log(emailEnder);
+  console.log(emailStart);
+  console.log(switchTWO);
+  console.log(switchONE);
+
   switch (switchTWO) {
-      case "high":
+      case "High":
         emailEnder = "@pagerduty.com"
         break;
-      case "medium":
+      case "Medium":
         emailEnder = "@zendesk.com"
         break;
-      case "low":
+      case "Low":
         emailEnder = "@spoonflower.com"
         break;
       default:
         console.log("Invalid response!");
+
+        console.log("step one" , emailEnder);
+        console.log("step one" , emailStart);
+        console.log("step one" , switchTWO);
+        console.log("step one" , switchONE);
     };
+
+
   switch (switchONE) {
       case "software or files":
         emailStart = "sysops" + emailEnder
@@ -118,8 +159,14 @@ function allTogether () {
         break;
       default:
         console.log("Invalid response!");
+
+        console.log("step two" , emailEnder);
+        console.log("step two" , emailStart);
+        console.log("step two" , switchTWO);
+        console.log("step two" , switchONE);
     };
   document.getElementById("finalScore").innerHTML = " "+ emailStart;
-  document.getElementById("ticketinfo").innerHTML = "Importance level is " + saveSeverity + ", impacting the " + machineTicket + ". ";
-  document.getElementById("otherinfo").innerHTML = natureOtherTicket + txtwrds;
+  document.getElementById("ticketstatus").innerHTML = "Alert level: <strong>" + saveSeverity + "</strong>"
+  document.getElementById("ticketinfo").innerHTML = machineTicket;
+  document.getElementById("otherinfo").innerHTML = natureOtherTicket + "<br>" + machineOther + "<br>" + txtwrds;
 }
