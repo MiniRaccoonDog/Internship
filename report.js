d65f080a739520987b13ef2116bcaa293f3776d9
Alyssa Torres <atorres@spoonflower.com>
2017-08-30 15:04:57 -0400

    Added .gitignore, remove Edit Reponse, fancy plus signs

--- report.js
+++ report.js
@@ -1,21 +1,40 @@
 $( document ).ready(function() {
   $( ".collapse" ).click(function() {
-    var target = $(this).attr("href");
+    console.log(this);
+    var togglethis = $(this).closest(".step").find(".reset");
+    var nextStep = $(this).parent().parent().next(".step");
+    toggleSection(togglethis);
+    /*
+    var target = $(this).attr("data-target");
     $(target).slideToggle("fast", function(){
       $(".response").show();
-    });
+      console.log(this);
+      toggleSection(togglethis);
+    })*/
+    $(nextStep).addClass("active");
   });
   $( ".reset" ).click(function() {
-    var target = $(this).attr("href");
-    $( target ).slideToggle( "slow", function() {
+    toggleSection(this);
     })
-  })
 });
+
+function toggleSection(element){
+  var questionToggle = $(element);
+  var target = $(element).attr("data-target");
+  questionToggle.closest(".step").find(".response").slideDown();
+  $( target ).slideToggle( "slow", function() {
+    questionToggle.find(".fa").toggle();
+  });
+};
+
 /*Establish Globals????*/
 var saveSeverity="";
 var saveNature="";
 var machineTicket="";
+var machineOther="";
 var natureOtherTicket="";
+var txtwrds="";
+var tablesumm="";
 /*For Question ONE*/
 function severityCheck() {
    var severity = document.forms[0].elements["severity"];
@@ -27,22 +46,52 @@ function severityCheck() {
      }
    };
    window.saveSeverity = sevtxt;
-   document.getElementById("priority").innerHTML = "Severity is: " + sevtxt;
+   document.getElementById("priority").innerHTML = "Severity level: " + sevtxt;
+};
+function fullSystemCheck() {
+  resetSystemCheck();
+  systemCheck();
 }
+
+/*Refresh Question TWO*/
+function resetSystemCheck() {
+    $("#rating").empty();
+    $("#rating2").empty();
+  }
+
 /*For Question TWO*/
 function systemCheck() {
-  var system = document.forms[0].elements["systems"];
-  var txt = "";
-  var txt2 = document.getElementById("systemfix").value;
-  var i;
-  for (i = 0; i < system.length; i++) {
-    if (system[i].checked) {
-      txt = txt + system[i].value + ", ";
+  var systems = document.forms[0].elements["systems"];
+  var txt2 = document.getElementById("systemsfix").value;
+  var finaltxt="";
+  var b;
+  var b2;
+  var sumtxt="";
+  for (b = 0; b < systems.length; b++) {
+    if (systems[b].checked) {
+      var selector = $(systems[b]).attr("data-target");
+      var subbies = $(selector).find(".subsystems");
+      var txt = systems[b].value
+      $("#rating").append("<h3>"+txt+"</h3><ul></ul>")
+      finaltxt += "<h3>"+txt+"</h3><ul>";
+      sumtxt += " |"+ txt +": ";
+      for (b2 = 0; b2 < subbies.length; b2++) {
+        if (subbies[b2].checked) {
+          var subtxt = subbies[b2].value
+          $("#rating").append("<li>"+subtxt+"</li>");
+          finaltxt += "<li>"+subtxt+"</li>";
+          sumtxt += subtxt + ", ";
+        }
+      }
+      finaltxt += "</ul>"
     }
   }
-  document.getElementById("rating").innerHTML = "Machines affected are: " + txt + txt2;
-  window.machineTicket = txt + txt2+". ";
+  $("#rating2").append("Other: "+txt2);
+  window.tablesumm = sumtxt;
+  window.machineTicket = finaltxt;
+  window.machineOther= txt2;
 }
+
 /*For Question THREE*/
 function natureCheck() {
   var nature = document.forms[0].elements["nature"];
@@ -54,45 +103,77 @@ function natureCheck() {
       txt3 = txt3 + nature[c].value;
     }
   }
-  console.log(txt3);
   window.saveNature = txt3;
-  document.getElementById("naturetype").innerHTML = "The problem appears to be " + txt3 +". "+ txt32;
-  window.natureOtherTicket = txt32+". ";
+  document.getElementById("naturetype").innerHTML = txt3 + ". " + txt32;
+  window.natureOtherTicket = txt32;
 };
-/*Okay lets try some bullshit */
+
+
+/*For Question FOUR*/
+function textareaCheck() {
+     window.txtwrds = document.getElementById("txtbox").value;
+   document.getElementById("texttArea").innerHTML = "Additional Notes: " + txtwrds;
+   var plz = document.getElementById("SECRETbox");
+   var plz2 = document.getElementById("SECRETbox2");
+   plz.value = "Additional Information: " + natureOtherTicket + ". " + machineOther + ". " + txtwrds;
+   plz2.value = tablesumm;
+};
+
+/*subQuestion Expander*/
+$( document ).ready(function() {
+  $(".Q3").change(function () {
+    var subs = $(this).attr("data-target");
+    if ($(subs).hasClass("active")) {
+      $(subs).removeClass("active")
+    } else {
+      $(subs).addClass("active");
+    }
+  })
+})
+
+
+/*Okay lets try some stuff */
 
 function allTogether () {
   var emailEnder = "";
   var emailStart = "";
   var switchTWO = saveSeverity;
   var switchONE = saveNature;
+  console.log(emailEnder);
+  console.log(emailStart);
+  console.log(switchTWO);
+  console.log(switchONE);
+
   switch (switchTWO) {
-      case "high":
+      case "High":
         emailEnder = "@pagerduty.com"
         break;
-      case "medium":
+      case "Medium":
         emailEnder = "@zendesk.com"
         break;
-      case "low":
+      case "Low":
         emailEnder = "@spoonflower.com"
         break;
       default:
         console.log("Invalid response!");
     };
+
+
   switch (switchONE) {
-      case "software or files":
+      case "Software or Files":
         emailStart = "sysops" + emailEnder
         break;
-      case "mechanical or physical":
+      case "Mechanical or Physical":
         emailStart = "maintenance"+ emailEnder
         break;
-      case "admin or website":
+      case "Admin or Website":
         emailStart = "plumbing"+ emailEnder
         break;
       default:
         console.log("Invalid response!");
     };
-  document.getElementById("finalScore").innerHTML = "Contact: "+ emailStart;
-  document.getElementById("ticketinfo").innerHTML = "Ticket is of " + saveSeverity + " importance, impacting the " + machineTicket
-  document.getElementById("otherinfo").innerHTML =  "Notes: " + natureOtherTicket
+  document.getElementById("finalScore").innerHTML = " "+ emailStart;
+  document.getElementById("ticketstatus").innerHTML = "Alert level: <strong>" + saveSeverity + "</strong>"
+  document.getElementById("ticketinfo").innerHTML = machineTicket;
+  document.getElementById("otherinfo").innerHTML = natureOtherTicket + "<br>" + machineOther + "<br>" + txtwrds;
 }
